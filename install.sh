53d2587f8725c00e78b7031aea0a240360453555
涵曦 <im.hanxi@gmail.com>
2021-11-29 16:15:16 +0800

    fix install again

--- install.sh
+++ install.sh
@@ -1,22 +1,29 @@
 set -x
 
+install_dir()
+{
+    rm -r "$2"
+    cp -rf "$1" "$2"
+}
+
 WLUA_BIN=$1
 WLUA_HOME=$2
 V_WLUA_HOME=${WLUA_HOME//\//\\\/} 
 INSTALL=/usr/bin/install
 ${INSTALL} -d ${WLUA_HOME}
-cp -rf conf ${WLUA_HOME}/conf
-cp -rf demo ${WLUA_HOME}/demo
+install_dir conf ${WLUA_HOME}/conf
+install_dir demo ${WLUA_HOME}/demo
 ${INSTALL} -p -D demo/.env ${WLUA_HOME}/demo/.env
-cp -rf luaclib ${WLUA_HOME}/luaclib
-cp -rf lualib ${WLUA_HOME}/lualib
-cp -rf service ${WLUA_HOME}/service
+install_dir luaclib ${WLUA_HOME}/luaclib
+install_dir lualib ${WLUA_HOME}/lualib
+install_dir service ${WLUA_HOME}/service
 ${INSTALL} -d ${WLUA_HOME}/skynet
-cp -rf skynet/cservice ${WLUA_HOME}/skynet/cservice
-cp -rf skynet/luaclib ${WLUA_HOME}/skynet/luaclib
-cp -rf skynet/lualib ${WLUA_HOME}/skynet/lualib
-cp -rf skynet/service ${WLUA_HOME}/skynet/service
+install_dir skynet/cservice ${WLUA_HOME}/skynet/cservice
+install_dir skynet/luaclib ${WLUA_HOME}/skynet/luaclib
+install_dir skynet/lualib ${WLUA_HOME}/skynet/lualib
+install_dir skynet/service ${WLUA_HOME}/skynet/service
 ${INSTALL} -p -D -m 0755 skynet/skynet ${WLUA_HOME}/skynet/skynet
 ${INSTALL} -p -D -m 0755 wlua ${WLUA_HOME}/wlua
 sed -i 's/REPLACE_INSTALL_PATH/'$V_WLUA_HOME'/g' ${WLUA_HOME}/wlua
 ln -sf ${WLUA_HOME}/wlua ${WLUA_BIN}
+
