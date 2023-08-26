a70ca11bf6192bef9a8e9696d732622783d5e23a
涵曦 <im.hanxi@gmail.com>
2023-08-26 17:30:39 +0800

    fix #3 : when args has nil value

--- lualib/log.lua
+++ lualib/log.lua
@@ -5,50 +5,51 @@ local traceback = debug.traceback
 
 local M = {}
 local levels = {
-    debug = 1,
-    info = 2,
-    warn = 3,
-    error = 4,
+	debug = 1,
+	info = 2,
+	warn = 3,
+	error = 4,
 }
 local level = config.get("wlua_loglevel", "debug")
 local loglevel = levels[level]
 
 function M.is_debug()
-    if levels.debug < loglevel then return end
-    return true
+	if levels.debug < loglevel then return end
+	return true
 end
 
 function M.debug(...)
-    if levels.debug < loglevel then return end
-    local tbl = {}
-    for i,v in pairs({...}) do
-        if type(v) == "table" then
-            tbl[i] = util_table.tostring(v)
-        else
-            tbl[i] = tostring(v)
-        end
-    end
-    skynet.error("[DEBUG]", table.concat(tbl, " "))
+	if levels.debug < loglevel then return end
+	local tbl = {}
+	for i = 1, select('#', ...) do
+		local v = select(i, ...)
+		if type(v) == "table" then
+			tbl[i] = util_table.tostring(v)
+		else
+			tbl[i] = tostring(v) or "nil"
+		end
+	end
+	skynet.error("[DEBUG]", table.concat(tbl, " "))
 end
 
 function M.info(...)
-    if levels.info < loglevel then return end
-    skynet.error("[INFO]", ...)
+	if levels.info < loglevel then return end
+	skynet.error("[INFO]", ...)
 end
 
 function M.warn(...)
-    if levels.warn < loglevel then return end
-    skynet.error("[WARN]", ...)
+	if levels.warn < loglevel then return end
+	skynet.error("[WARN]", ...)
 end
 
 function M.error(...)
-    if levels.error < loglevel then return end
-    skynet.error("[ERROR]", ...)
-    skynet.error("[ERROR]", traceback())
+	if levels.error < loglevel then return end
+	skynet.error("[ERROR]", ...)
+	skynet.error("[ERROR]", traceback())
 end
 
 function M.log(...)
-    skynet.error(...)
+	skynet.error(...)
 end
 
 return M
